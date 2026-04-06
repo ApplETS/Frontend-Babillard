6af80bf23d8b2aa7e9c7020b5ae47c31516d6e68
superjekk <superjekk@outlook.com>
2026-04-06 17:56:08 -0400

    PROGRES : Création de dashboard

--- src/app/routed-components/dashboard-news/dashboard-news.ts
+++ src/app/routed-components/dashboard-news/dashboard-news.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { Calendar } from "../../components/calendar/calendar";
 import { Publications } from "../../components/publications/publications";
 import { LoadingSpinner } from "../../components/loading-spinner/loading-spinner";
+import { Dashboard } from "../../components/dashboard/dashboard";
 
 @Component({
   selector: 'app-dashboard-news',
-  imports: [Calendar, Publications, LoadingSpinner],
+  imports: [Calendar, Publications, LoadingSpinner, Dashboard],
   templateUrl: './dashboard-news.html',
 })
 export class DashboardNews {}
