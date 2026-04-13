442589c8fdd9d33c3583b2b60f0aa93112b91bd1
Matai <code@matai.info>
2026-04-13 17:59:50 -0400

    import old translations

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,9 +2,11 @@ import { ApplicationConfig, provideBrowserGlobalErrorListeners, isDevMode } from
 import { provideRouter } from '@angular/router';
 import { provideHttpClient } from '@angular/common/http';
 import { provideTransloco } from '@jsverse/transloco';
+import { provideTranslocoPersistTranslations } from '@jsverse/transloco-persist-translations';
 
 import { routes } from './app.routes';
 import { TranslocoHttpLoader } from './core/systems/i18n/transloco-loader';
+import { provideTranslocoLocale } from '@jsverse/transloco-locale';
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -19,6 +21,12 @@ export const appConfig: ApplicationConfig = {
       },
       loader: TranslocoHttpLoader,
     }),
+    provideTranslocoPersistTranslations({
+      loader: TranslocoHttpLoader, // Auto-generated via ng add
+      storage: { useValue: localStorage },
+    }),
+    provideTranslocoLocale(),
+
     provideRouter(routes),
   ],
 };
