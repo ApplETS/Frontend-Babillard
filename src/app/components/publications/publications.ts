ac46a31067f2d58a22a2ee189f2dc89775096b06
superjekk <superjekk@outlook.com>
2026-06-15 17:44:38 -0400

    Transfert de CardRotation vers le client Angular

--- src/app/components/publications/publications.ts
+++ src/app/components/publications/publications.ts
@@ -1,12 +1,18 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, model } from '@angular/core';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faDownLeftAndUpRightToCenter, faUpRightAndDownLeftFromCenter } from '@fortawesome/free-solid-svg-icons';
 import { Event } from '@models/event';
 
 @Component({
   selector: 'app-publications',
-  imports: [],
+  imports: [FontAwesomeModule],
   templateUrl: './publications.html',
   styles: ``,
 })
 export class Publications {
   @Input({ required: true }) events: Event[] | null = [];
+  selectedCard = model<number | null>(null);
+
+  readonly faDownLeftAndUpRightToCenter = faDownLeftAndUpRightToCenter;
+  readonly faUpRightAndDownLeftFromCenter = faUpRightAndDownLeftFromCenter;
 }
