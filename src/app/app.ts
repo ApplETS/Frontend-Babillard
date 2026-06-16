13b1280509769a87abaa991dad6104f19017bb73
superjekk <superjekk@outlook.com>
2026-06-15 22:28:00 -0400

    Déplacement de composants d'en-tête

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,8 +1,5 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { TranslocoPipe } from '@jsverse/transloco';
-import { LangSwitcher } from './components/lang-switcher/lang-switcher';
-import { ThemeSwitcher } from './components/theme-switcher/theme-switcher';
 import { HeaderComponent } from "@components/header/header";
 
 @Component({
--- src/app/components/header/header.ts
+++ src/app/components/header/header.ts
@@ -2,10 +2,12 @@ import { Component } from '@angular/core';
 import { ThemeSwitcher } from "../theme-switcher/theme-switcher";
 import { faSignIn} from "@fortawesome/free-solid-svg-icons"
 import { FontAwesomeModule } from "@fortawesome/angular-fontawesome";
+import { LangSwitcher } from "@components/lang-switcher/lang-switcher";
+import { TranslocoPipe } from "@jsverse/transloco";
 
 @Component({
   selector: 'app-header',
-  imports: [ThemeSwitcher, FontAwesomeModule],
+  imports: [ThemeSwitcher, FontAwesomeModule, LangSwitcher, TranslocoPipe],
   templateUrl: './header.html',
 })
 export class HeaderComponent {
