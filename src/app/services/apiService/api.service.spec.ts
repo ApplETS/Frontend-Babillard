cba968b4504625fb85919826bf257a5e1ad102ae
superjekk <superjekk@outlook.com>
2026-09-03 13:48:58 -0400

    Ajout de la méthode d'appel paginé et documentation de méthodes

--- src/app/services/apiService/api.service.spec.ts
+++ src/app/services/apiService/api.service.spec.ts
@@ -1,6 +1,6 @@
 import { TestBed } from '@angular/core/testing';
 
-import { ApiService } from "./api.service";
+import { ApiService, PaginatedResponse } from "./api.service";
 import { HttpParams, provideHttpClient } from '@angular/common/http';
 import { HttpTestingController, provideHttpClientTesting } from '@angular/common/http/testing';
 import { describe, vi } from 'vitest';
@@ -21,6 +21,10 @@ export class TestApiService extends ApiService{
 
   public override async get<T>(action: string, routeParameters: unknown[], queryParameters: HttpParams): Promise<T> {
     return await super.get<T>(action, routeParameters, queryParameters);
+  }
+
+  public override async getPaginated<T>(action: string, pageNumber: number, pageSize: number, ...routeParameters: unknown[]): Promise<PaginatedResponse<T>> {
+    return await super.getPaginated<T>(action, pageNumber, pageSize, ...routeParameters);
   }
 }
 
@@ -81,5 +85,29 @@ describe('ApiService', () => {
     req.flush("response");
 
     await expect(responsePromise).resolves.toBe("response");
-  })
+  });
+
+  it("should call httpService.get with the correct url and query parameters for getPaginated", async () => {
+    const action = "testAction";
+    const pageNumber = 1;
+    const pageSize = 10;
+    const routeParameters = ["param1", "param2"];
+    const expectedUrl = `${environment.API_URL}/api/test/${action}/${routeParameters.join("/")}`;
+    const responsePromise = service.getPaginated<string>(service.getActionUrl(action), pageNumber, pageSize, ...routeParameters);
+
+    const req = httpServiceSpy.expectOne((request) => {
+      return request.method === 'GET' && request.url === expectedUrl && request.params.get('page') === pageNumber.toString() && request.params.get('pageSize') === pageSize.toString();
+    }, "GET request for getPaginated");
+
+    const response = {
+      data: ["item1", "item2"],
+      pageSize: pageSize,
+      totalRecords: 2,
+      totalPages: 1,
+      error: null
+    };
+    req.flush(response);
+
+    await expect(responsePromise).resolves.toEqual(response);
+  });
 });
--- src/app/services/apiService/api.service.ts
+++ src/app/services/apiService/api.service.ts
@@ -17,6 +17,14 @@ export abstract class ApiService {
     return `${environment.API_URL}/api/${this.apiController}/${action}`;
   }
 
+  /**
+   * Fetches data from the API via the GET method and adds the access token to the request headers if the user is authenticated.
+   * @template T Type of the data to be returned
+   * @param action The endpoint's action to call
+   * @param routeParameters Parameters to add to the endpoint path
+   * @param queryParameters Parameters to add to the endpoint as query parameters
+   * @returns T type result from the API
+   */
   protected async get<T>(action: string, routeParameters: unknown[] = [], queryParameters: HttpParams = new HttpParams()): Promise<T> {
     if (!action.endsWith("/") && routeParameters.length > 0) {
       action += "/";
@@ -32,8 +40,33 @@ export abstract class ApiService {
 
     return await lastValueFrom(this.httpService.get<T>(action, { params: queryParameters, headers: headers }));
   }
+
+  /**
+   * Fetches a paginated response from the API via the GET method.
+   * @template T Type of the data in the paginated response
+   * @param action Action to access
+   * @param pageNumber Page number to paginated list
+   * @param pageSize Page size of paginated list
+   * @param routeParameters Parameters to append to the path
+   * @returns A PaginatedResponse of type T
+   * @see get<T>
+   * @see PaginatedResponse<T>
+   */
+  protected async getPaginated<T>(action: string, pageNumber: number, pageSize: number, ...routeParameters: unknown[]) {
+    return await this.get<PaginatedResponse<T>>(action, routeParameters, new HttpParams({
+      fromObject: {
+        page: pageNumber,
+        pageSize: pageSize,
+      }
+    }));
+  }
 }
 
+/**
+ * Represents a paginated response from the API.
+ * @template T Type of the data in the paginated response
+ * @property {T[]} data The data returned from the API
+ */
 export interface PaginatedResponse<T> {
   data: T[];
   pageSize: number;
