d2b4b09cfd4b9caa81e6d6ecabfa91d1965e189d
superjekk <superjekk@outlook.com>
2026-09-08 15:08:01 -0400

    Tests de vérification du formatage de paramètres

--- src/app/services/dashboard.service/events.service.spec.ts
+++ src/app/services/dashboard.service/events.service.spec.ts
@@ -6,6 +6,8 @@ import { environment } from '@environments/environment';
 import { of } from 'rxjs';
 import { OidcSecurityService } from 'angular-auth-oidc-client';
 import { EventsService } from './events.service';
+import { PaginatedResponse } from '@services/apiService/api.service';
+import { Event } from '@models/event';
 
 describe('EventService', () => {
   let service: EventsService;
@@ -38,26 +40,75 @@ describe('EventService', () => {
     expect(service).toBeTruthy();
   });
 
-  it('should return events and call without activityTags', async () => {
+  it('should return events and call without activityArea', async () => {
     const expectedPath = `${environment.API_URL}/api/events/?pageNumber=1&pageSize=1000`;
    
     const response = service.getEvents([]);
     const request = httpClientSpy.expectOne(expectedPath);
     expect(request.request.method).toBe('GET');
 
-    const mockResponse = {
+    const mockResponse: PaginatedResponse<Event> = {
       data: [
-        { id: '1', nameFr: 'Nom FR', nameEn: 'Name EN' }
+        { id: '1', title: 'Event 1', content: 'Content 1', imageUrl: 'url1', imageAltText: 'alt1', eventStartDate: '2024-01-01', eventEndDate: '2024-01-02', organizer: null, tags: [{id: "tag1", name: "tag", createdAt: "", updatedAt: ""}] }
       ],
+      pageSize: 1000,
+      totalRecords: 1,
+      totalPages: 1,
       error: null
     };
 
     request.flush(mockResponse);
     
     const result = await response;
-    expect(result).toEqual([
-      { id: '1', name: 'Name EN', selected: true }
-    ]);
+    expect(result).toEqual(mockResponse);
+  });
+
+  it('should return events and call with single activityArea', async () => {
+    const activityAreas = ['tag1'];
+    const expectedPath = `${environment.API_URL}/api/events/?pageNumber=1&pageSize=1000&activityAreas=tag1`;
+
+    const response = service.getEvents(activityAreas);
+    const request = httpClientSpy.expectOne(expectedPath);
+    expect(request.request.method).toBe('GET');
+
+    const mockResponse: PaginatedResponse<Event> = {
+      data: [
+        { id: '1', title: 'Event 1', content: 'Content 1', imageUrl: 'url1', imageAltText: 'alt1', eventStartDate: '2024-01-01', eventEndDate: '2024-01-02', organizer: null, tags: [{id: "tag1", name: "tag", createdAt: "", updatedAt: ""}] }
+      ],
+      error: null,
+      pageSize: 1000,
+      totalRecords: 1,
+      totalPages: 1
+    };
+
+    request.flush(mockResponse);
+
+    const result = await response;
+    expect(result).toEqual(mockResponse);
+  });
+
+    it('should return events and call with multiple activityAreas', async () => {
+    const activityAreas = ['tag1', 'tag2'];
+    const expectedPath = `${environment.API_URL}/api/events/?pageNumber=1&pageSize=1000&activityAreas=tag1&activityAreas=tag2`;
+
+    const response = service.getEvents(activityAreas);
+    const request = httpClientSpy.expectOne(expectedPath);
+    expect(request.request.method).toBe('GET');
+
+    const mockResponse: PaginatedResponse<Event> = {
+      data: [
+        { id: '1', title: 'Event 1', content: 'Content 1', imageUrl: 'url1', imageAltText: 'alt1', eventStartDate: '2024-01-01', eventEndDate: '2024-01-02', organizer: null, tags: [{id: "tag1", name: "tag", createdAt: "", updatedAt: ""}] }
+      ],
+      error: null,
+      pageSize: 1000,
+      totalRecords: 1,
+      totalPages: 1
+    };
+
+    request.flush(mockResponse);
+
+    const result = await response;
+    expect(result).toEqual(mockResponse);
   });
 
   afterEach(() => {
