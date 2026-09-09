0f11b597df8d664af0eb527ca2d0e9dcc5154395
superjekk <superjekk@outlook.com>
2026-09-09 14:42:33 -0400

    Localisation des dates dans le calendrier

--- src/app/components/calendar-header/calendar-header.ts
+++ src/app/components/calendar-header/calendar-header.ts
@@ -4,13 +4,13 @@ import { TimeGridType, CalendarAction } from '@components/calendar/calendar';
 import { DropDownSelectComponent } from '@components/drop-down-select.component/drop-down-select.component';
 import { FaIconComponent } from "@fortawesome/angular-fontawesome";
 import { faChevronLeft, faChevronRight, faCalendarDay } from '@fortawesome/free-solid-svg-icons';
-import { TranslocoPipe } from '@jsverse/transloco';
+import { TranslocoPipe, TranslocoService } from '@jsverse/transloco';
 import { TranslocoDatePipe } from '@jsverse/transloco-locale';
 import { ActivityAreaDisplay } from '@services/activityAreaService/activity-area.service';
 
 @Component({
   selector: 'app-calendar-header',
-  imports: [FaIconComponent, DropDownSelectComponent, CommonModule, TranslocoPipe],
+  imports: [FaIconComponent, DropDownSelectComponent, CommonModule, TranslocoPipe, TranslocoDatePipe],
   templateUrl: './calendar-header.html',
 })
 export class CalendarHeader {
@@ -24,7 +24,7 @@ export class CalendarHeader {
   @Input({required: true}) currentDate!: moment.Moment;
   activityAreas = model<ActivityAreaDisplay[] | null>(null);
   @Output() calendarChanged = new EventEmitter<CalendarAction>();
-  translationService = inject(TranslocoDatePipe);
+  translateService = inject(TranslocoService);
 
   handleDateChange(action: CalendarAction): void {
     this.calendarChanged.emit(action);
@@ -32,22 +32,5 @@ export class CalendarHeader {
 
   handleViewChange(view: TimeGridType): void {
     this.viewType.set(view);
-  }
-
-  get timeFrame(): string {
-    // TODO : Utiliser un format à partir de la localisation client
-    switch (this.viewType()) {
-      case TimeGridType.month:
-        return this.currentDate.format("MMMM YYYY");
-        
-      case TimeGridType.week:
-        const startOfWeek = this.currentDate.clone().startOf('week').format("D MMM");
-        const endOfWeek = this.currentDate.clone().endOf('week').format("D MMM YYYY");
-        return `${startOfWeek} - ${endOfWeek}`;
-      case TimeGridType.day:
-        return this.currentDate.format("D MMM YYYY");
-      default:
-        return "";
-    }
   }
 }
