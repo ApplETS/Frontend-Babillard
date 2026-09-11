07eb180a705f74bac76414adcb7848381c1b674e
superjekk <superjekk@outlook.com>
2026-09-11 13:12:05 -0400

    PROGRES : Prise en compte des routes disponibles

--- src/app/components/header/header.ts
+++ src/app/components/header/header.ts
@@ -1,15 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, computed, inject } from '@angular/core';
 import { ThemeSwitcher } from "../theme-switcher/theme-switcher";
 import { faSignIn} from "@fortawesome/free-solid-svg-icons"
-import { FontAwesomeModule } from "@fortawesome/angular-fontawesome";
+import { FaIconComponent } from "@fortawesome/angular-fontawesome";
 import { LangSwitcher } from "@components/lang-switcher/lang-switcher";
 import { TranslocoPipe } from "@jsverse/transloco";
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-header',
-  imports: [ThemeSwitcher, FontAwesomeModule, LangSwitcher, TranslocoPipe],
+  imports: [ThemeSwitcher, FaIconComponent, LangSwitcher, TranslocoPipe],
   templateUrl: './header.html',
 })
 export class HeaderComponent {
   signIn = faSignIn;
+
+  router = inject(Router);
+
+  routes = computed(() => {
+    return [];
+  })
 }
