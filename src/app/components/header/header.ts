704c0725042a79c9a291532c310652ed95ec3d6a
superjekk <superjekk@outlook.com>
2026-09-15 13:29:32 -0400

    Portage de la section d'en-tête

--- src/app/components/header/header.ts
+++ src/app/components/header/header.ts
@@ -1,14 +1,16 @@
-import { Component, computed, inject, OnInit, Signal } from '@angular/core';
+import { Component, computed, inject, OnInit, signal, Signal } from '@angular/core';
 import { ThemeSwitcher } from "../theme-switcher/theme-switcher";
-import { faSignIn} from "@fortawesome/free-solid-svg-icons"
+import { faSignIn, faGear, faSignOut } from "@fortawesome/free-solid-svg-icons"
 import { FaIconComponent } from "@fortawesome/angular-fontawesome";
 import { LangSwitcher } from "@components/lang-switcher/lang-switcher";
-import { TranslocoPipe } from "@jsverse/transloco";
+import { TranslocoPipe, TranslocoService } from "@jsverse/transloco";
 import { ActivatedRoute, isActive, Router, RouterLink } from '@angular/router';
 import { OidcSecurityService } from 'angular-auth-oidc-client';
 import { AuthService } from '@services/authService/auth.service';
 import { UserType } from '@models/user-types';
 import { ImgWithPlaceholder } from '@components/img-with-placeholder/img-with-placeholder';
+import { Avatar } from '@components/avatar/avatar';
+import { User } from '@models/user';
 
 type RouteInfo = {
   route: string;
@@ -18,15 +20,19 @@ type RouteInfo = {
 
 @Component({
   selector: 'app-header',
-  imports: [ThemeSwitcher, FaIconComponent, LangSwitcher, TranslocoPipe, RouterLink, ImgWithPlaceholder],
+  imports: [ThemeSwitcher, FaIconComponent, LangSwitcher, TranslocoPipe, RouterLink, ImgWithPlaceholder, Avatar],
   templateUrl: './header.html',
 })
 export class HeaderComponent implements OnInit {
   signIn = faSignIn;
+  readonly gear = faGear;
+  readonly signOut = faSignOut;
 
   router = inject(Router);
   securityService = inject(OidcSecurityService);
   authService = inject(AuthService);
+  translocoService = inject(TranslocoService);
+  dropdownToggled = signal(false);
 
   routes = computed<RouteInfo[]>(() => {
     if (!this.authService.isAuthenticated()) {
@@ -54,4 +60,25 @@ export class HeaderComponent implements OnInit {
   ngOnInit(): void {
     this.authService.initAuth();
   }
+
+  organization = computed(() => {
+    const user = this.authService.userInfo();
+    if (!user) {
+      return "";
+    }
+
+    return user.type?.match(UserType.MODERATOR) ? this.translocoService.translate("moderator") : user.organization;
+  });
+
+  area = computed(() => {
+    const user = this.authService.userInfo();
+    if (!user) {
+      return "";
+    }
+
+    if (this.translocoService.activeLang() === "fr") {
+      return user.activityArea?.nameFr ?? "";
+    }
+    return user?.activityArea?.nameEn ?? "";
+  });
 }
