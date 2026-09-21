129947c3c0f4ecb50ad61c422994c3561772942f
superjekk <superjekk@outlook.com>
2026-09-21 17:11:04 -0400

    Création de composant spécialisé pour les publications

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,5 +2,6 @@ import { Routes } from '@angular/router';
 
 export const routes: Routes = [
   { path: "dashboard/news", loadComponent: () => import('@routed-components/dashboard-news/dashboard-news').then(m => m.DashboardNews) },
+  { path: "posts", loadComponent: () => import('@routed-components/posts/posts').then(m => m.Posts) },
   { path: '**', redirectTo: '/dashboard/news' }
 ];
---
+++ src/app/routed-components/posts/posts.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-posts',
+  imports: [],
+  templateUrl: './posts.html',
+})
+export class Posts {
+  
+}
