cdb18759694cc1dc8de4b744ff727849e2c75d67
superjekk <superjekk@outlook.com>
2026-09-22 15:04:06 -0400

    Début de mise en place d'interface
    L'en-tête de la liste paginée commence

--- src/app/routed-components/posts/posts.ts
+++ src/app/routed-components/posts/posts.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
+import { TranslocoDirective } from '@jsverse/transloco';
+import { DropDownSelectComponent } from '@components/drop-down-select.component/drop-down-select.component';
 
 @Component({
   selector: 'app-posts',
-  imports: [],
+  imports: [TranslocoDirective, DropDownSelectComponent],
   templateUrl: './posts.html',
 })
 export class Posts {
-  
+
 }
